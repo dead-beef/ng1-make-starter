5fb77b9ce3f7fdbf94fffee5f3f5a457bdddd94e
dead-beef <dead-beef@users.noreply.github.com>
2017-10-30 11:20:21 +0000

    change makefile variable generator

--- config/main-files.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	'angular': 'angular.js',
-	'@uirouter/core': '_bundles/ui-router-core.js',
-	'@uirouter/angularjs': 'release/ui-router-angularjs.js',
-	'angular-animate': 'angular-animate.js',
-	'angular-aria': 'angular-aria.js',
-	'angular-material': 'angular-material.js',
-	'angular-cookies': 'angular-cookies.js'
-};
